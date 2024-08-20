def426352e9e37785afae52f3c162f26e7a2e9e9
EvatsugDnalloR <gustaverolland777@gmail.com>
2024-08-20 21:46:03 +0200

    tried to handle Ctrl + C signal but did not success

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+#include <csignal>
+#include <cstdlib>
 #include "ToDoList/MainFrame.h"
+
+// Signal handler function
+void signalHandler(int signum) {
+	std::cout << "\nInterrupt signal (" << signum << ") received.\n";
+	// Perform cleanup and exit
+	std::cout << "Performing cleanup...\n";
+	// Add your cleanup code here
+	exit(signum);
+}
 
 int main(int argc, char* argv[])
 {
+	signal(SIGINT, signalHandler);
 	MainFrame main_frame;
 	main_frame.StartApp();
 	if (!main_frame.GetExitSuccess())
