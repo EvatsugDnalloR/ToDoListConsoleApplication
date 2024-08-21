03ebf75877a7e0eb3e2e27d0af8108072d3133a5
EvatsugDnalloR <gustaverolland777@gmail.com>
2024-08-21 15:54:05 +0200

    kind of handled Ctrl C signal

--- main.cpp
+++ main.cpp
@@ -2,23 +2,26 @@
 #include <cstdlib>
 #include "ToDoList/MainFrame.h"
 
-// Signal handler function
-void signalHandler(int signum) {
-	std::cout << "\nInterrupt signal (" << signum << ") received.\n";
-	// Perform cleanup and exit
-	std::cout << "Performing cleanup...\n";
-	// Add your cleanup code here
-	exit(signum);
+/**
+ * Signal handler function, once the Ctrl C pressed, the
+ *   program will just freeze and require manual exit.
+ * @param sig_num	signal number sent through Ctrl C
+ */
+void SignalHandler(const int sig_num)
+{
+	std::println("{}{}\nInterrupt signal ({}) received",kBold, kRed, sig_num);
+	std::print("Program will be frozen, please quit the program manually{}", kReset);
+	exit(sig_num);
 }
 
 int main(int argc, char* argv[])
 {
-	signal(SIGINT, signalHandler);
+	signal(SIGINT, SignalHandler);  // NOLINT(cert-err33-c), register the signal handler
 	MainFrame main_frame;
 	main_frame.StartApp();
 	if (!main_frame.GetExitSuccess())
 	{
-		return EXIT_FAILURE;
+		return EXIT_FAILURE;	// mostly due to file missing or file corruption of specification.txt
 	}
 	return EXIT_SUCCESS;
 }
